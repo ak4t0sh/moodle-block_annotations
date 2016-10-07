087fa7316a712ba743bbf2a08fcf32ec187f022b
ak4t0sh <ak4t0sh@free.fr>
2016-10-08 01:11:26 +0200

    implementing and testing amd module

--- amd/src/annotations.js
+++ amd/src/annotations.js
@@ -1,4 +1,3 @@
-<?php
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -14,40 +13,28 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Handles displaying the annotation block.
- *
+/*
  * @package    block_annotations
  * @copyright  2016 Arnaud Trouvé <ak4t0sh@free.fr>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class block_annotations extends block_base {
 
-    /**
-     * Initialise the block.
-     */
-    public function init() {
-        $this->title = get_string('pluginname', 'block_annotations');
+/**
+ * @module block_annotations/annotations
+ */
+define(['jquery'], function($) {
+    "use strict";
+    function init() {
+        $('ul.section li.activity > div').each(function() {
+                $(this).append('<div class="annotations"><a href="#" class="btn">Annotations</a></div>');
+            }
+        );
+        $('.annotations a.btn').click(function() {
+            // jshint devel:true
+            console.log($(this).closest('li.activity').attr('id'));
+        });
     }
-
-    /**
-     * Return the content of this block.
-     *
-     * @return stdClass the content
-     */
-    public function get_content() {
-
-        if ($this->content !== null) {
-            return $this->content;
-        }
-
-        $this->page->requires->js_call_amd('block_annotations/annotations', 'init');
-        $this->content = new stdClass();
-        $this->content->text = 'test';
-        $this->content->footer = '';
-
-        return $this->content;
-    }
-}
-
-
+    return {
+        init: init
+    };
+});
