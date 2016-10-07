468015c9ea169671e217fe51f5f56f6718afa26b
ak4t0sh <ak4t0sh@free.fr>
2016-10-07 23:34:20 +0200

    use short array syntax

--- db/access.php
+++ db/access.php
@@ -24,28 +24,24 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$capabilities = array(
-
-    'block/annotations:myaddinstance' => array(
+$capabilities = [
+    'block/annotations:myaddinstance' => [
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
-        'archetypes' => array(
+        'archetypes' => [
             'user' => CAP_ALLOW
-        ),
-
+        ],
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
-    ),
-
-    'block/annotations:addinstance' => array(
+    ],
+    'block/annotations:addinstance' => [
         'riskbitmask' => RISK_SPAM | RISK_XSS,
 
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
-        'archetypes' => array(
+        'archetypes' => [
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
-        ),
-
+        ],
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
-    ),
-);
+    ],
+];
